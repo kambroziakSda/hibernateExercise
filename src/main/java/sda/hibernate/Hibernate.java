b4b30a9402d29a012d8fae6ca7fa9db2ee5e8b52
Krzysztof Ambroziak <krzysztof.ambroziak2@bm.pl>
2021-02-24 14:23:23 +0100

    persist

--- src/main/java/sda/hibernate/Hibernate.java
+++ src/main/java/sda/hibernate/Hibernate.java
@@ -2,7 +2,10 @@ package sda.hibernate;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+
+import java.io.Serializable;
 
 public class Hibernate {
 
@@ -12,13 +15,34 @@ public class Hibernate {
                 .addAnnotatedClass(Student.class)
                 .buildSessionFactory()) {
 
-            Student student = new Student("Jan", "Kowalski", new Address("Gdańsk","Grunwaldzka"));
 
             try (Session session = sessionFactory.openSession()) {
 
+            //jpa api
+                Transaction transaction = session.beginTransaction();
+                Student studentJan = new Student("Jan", "Kowalski", new Address("Gdańsk", "Grunwaldzka"));
+
+                /*
+                tutaj nie ma jescze zapisu, hibernate moze nawet sqli nie wysłac do bazy,
+                kiedy sqle wysyłane sa do bazy sterowane jest przez FlushMode
+                https://docs.jboss.org/hibernate/orm/5.2/javadocs/org/hibernate/FlushMode.html
+                 */
+
+                session.persist(studentJan);
+
+                System.out.println("Before commit");
+                transaction.commit(); //zapis do bazy dopiero tutaj
+
+
+            //hibernate api
+                transaction = session.beginTransaction();
+                Student studentAla = new Student("Ala", "Kowalska", new Address("Gdańsk", "Grunwaldzka"));
+                Serializable id = session.save(studentAla);
+                transaction.commit();
+
+                System.out.println(id);
+
             }
-
-
         }
 
     }
--- src/main/java/sda/hibernate/Student.java
+++ src/main/java/sda/hibernate/Student.java
@@ -1,21 +1,27 @@
 package sda.hibernate;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
 
-//@Entity(name = "StudentEntity") Adnotacja entity pozwala na zmiane domyślnej nazwy ecnji (uwaga: nie mylić z nazwą tabeli w bazie danych)
-//@Table(name = "tabstudent")
 @Entity
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
-    //@GeneratedValue(strategy = GenerationType.SEQUENCE)
-    //@SequenceGenerator()
     private Integer id;
 
     @Column(name = "name") //adnotacja @Column zmienia domyślną definicje pola
     private String firstName;
     private String lastName;
+
+    private LocalDateTime createTime;
+
+    @PrePersist
+    void prePersist(){
+        System.out.println("Pre persist");
+        createTime = LocalDateTime.now();
+    }
+
 
     @Embedded
     private Address address;
@@ -26,33 +32,14 @@ public class Student {
         this.address = address;
     }
 
-    /*
-            Getery i settery nie są konieczne, hibernate uzywa pol klasy i mechanizmu refleksji w celu ustawienia
-            wartości dla poszczegolnych pól, chyba że adnotacje modelujące encje bedziemy umieszczać nad geterami,
-            wtedy zarowno gettery jak i settery do pól są konieczne, nie można pomieszać tych podejść czyli umieścić
-            cześć adnotacji nad polami a część nad geterami
-               */
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
     @Override
     public String toString() {
         return "Student{" +
-                "firstName='" + firstName + '\'' +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
                 ", lastName='" + lastName + '\'' +
+                ", createTime=" + createTime +
+                ", address=" + address +
                 '}';
     }
 }
