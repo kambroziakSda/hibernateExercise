ad90c377756cc70e572ff83e373f547332636833
Krzysztof Ambroziak <krzysztof.ambroziak2@bm.pl>
2021-02-25 11:25:19 +0100

    Uzupełnienie update o session jako cache

--- src/main/java/sda/hibernate/Hibernate.java
+++ src/main/java/sda/hibernate/Hibernate.java
@@ -24,8 +24,28 @@ public class Hibernate {
                 Student studentJan = session.find(Student.class, 1);
                 System.out.println("Student from database: " + studentJan);
                 studentJan.setAddress(new Address("Warszawa","Miodowa"));
-                transaction.commit();
+                transaction.commit(); //zapis do bazy dopiero tutaj
+            }
 
+            /*
+            Poniższy przykład pokazuje działanie obiektu sesji jako cache dla encji. Aktualizacja danych jest widoczna
+            po drugim wywołaniu find mimo że nie było zapisu do bazy. Obiekt jest zmieniany na poziomie sesji i nastepnie z niej wyciagany.
+            Porownanie .equals pokazuje ze find 1 i find 2 zwraca ten sam obiekt
+             */
+
+            try (Session session = sessionFactory.openSession()) {
+                //update wymaga transakcji tak samo jak insert
+                Transaction transaction = session.beginTransaction();
+                Student studentJan = session.find(Student.class, 1); //find 1
+                System.out.println("Student from database after 1 update: " + studentJan);
+                studentJan.setAddress(new Address("Warszawa","Wiejska"));
+
+                Student studentJan2 = session.find(Student.class, 1); //find 2
+
+                System.out.println("Student from database after 2 update: " + studentJan2);
+                System.out.println("Czy to ten sam obiekt studenta: " + studentJan.equals(studentJan2));
+
+                transaction.commit(); //zapis do bazy dopiero tutaj
             }
         }
 
